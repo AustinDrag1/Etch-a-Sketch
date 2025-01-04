8ccd71f5e8d4399cfeb7e169a77a52771c202acc
Austin Dragone <18505590+AustinDrag1@users.noreply.github.com>
2025-01-04 14:02:27 -0500

    Created grid initialize and reset functionality

---
+++ script.js
@@ -0,0 +1,44 @@
+// Event listener for button to prompt user for new grid size
+const btn = document.querySelector("#new");
+btn.addEventListener("click", () => {
+    newGridRequested( false );
+})
+
+// Get grid size, create grid, and replace existing grid
+function newGridRequested( init )
+{
+    const size = init ? 16 : prompt("What size grid do you want?");
+    const newGrid = createGrid( size );
+    replaceGrid( newGrid );
+}
+
+// Create X^2 elements for grid
+function createGrid( num )
+{
+    const grid = [];
+    const containerWidth = 960;
+    const borderConsideration = 2;
+    let width = 960 / num - borderConsideration;
+
+    for( let i = 0; i < num * num; i++ )
+    {
+        let sqr = document.createElement("div");
+        sqr.classList.add("sqr");
+        sqr.style.flexBasis = width + "px";
+        sqr.style.height = width + "px";
+        grid.push(sqr);
+    }
+    
+    return grid;
+}
+
+// Reset grid - clear everything out of the primary container
+function replaceGrid( newGrid )
+{
+    console.log(newGrid);
+    const gridContainer = document.querySelector("#grid-container");
+    gridContainer.replaceChildren(...newGrid);
+}
+
+// Initialize 16x16 grid
+newGridRequested( 16 );
