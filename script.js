c26533d96bc2c90dd037f522d65cc2a1e61601dd
Austin Dragone <18505590+AustinDrag1@users.noreply.github.com>
2025-01-04 14:20:40 -0500

    added draw feature for coloring individual squares (no drag)

--- script.js
+++ script.js
@@ -4,10 +4,23 @@ btn.addEventListener("click", () => {
     newGridRequested( false );
 })
 
+// Event listener for the draw action
+const drawArea = document.querySelector("#grid-container");
+drawArea.addEventListener("mousedown", (e) => {
+    draw(e);
+});
+
 // Get grid size, create grid, and replace existing grid
 function newGridRequested( init )
 {
     const size = init ? 16 : prompt("What size grid do you want?");
+
+    if( size > 100 || size < 1 )
+    {
+        alert("Please keep the range between 1 and 100");
+        return;
+    }
+
     const newGrid = createGrid( size );
     replaceGrid( newGrid );
 }
@@ -38,6 +51,12 @@ function replaceGrid( newGrid )
     console.log(newGrid);
     const gridContainer = document.querySelector("#grid-container");
     gridContainer.replaceChildren(...newGrid);
+}
+
+// Color clicked square
+function draw(e)
+{
+    e.target.style.backgroundColor = "black";
 }
 
 // Initialize 16x16 grid
